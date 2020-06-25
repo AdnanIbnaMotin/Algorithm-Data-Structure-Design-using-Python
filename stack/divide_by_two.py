9201dc3e304a8c2fbb0efb79d776f19f25d78b0e
Adnan Rahman <adnanibnamotin@gmail.com>
2020-06-25 14:15:58 -0400

    divide by 2 algorithm implementation

---
+++ stack/divide_by_two.py
@@ -0,0 +1,22 @@
+"""
+"Divide by Two" is an algorithm which is used to convert a decimal number into binary using stack data structure.
+The idea behind the "Devide by Two" algorithm is to divide the number each time by 2 and push the reminder into the
+stack and the reverse order will be the binary number of that decimal number
+"""
+from stack_1 import Stack
+
+def devideByTwo(number):
+
+    s = Stack()
+    while number // 2 != 0:
+        s.push(number % 2)
+        number //= 2
+    s.push(1)
+
+    numberToStr = ""
+    while s.size() > 0:
+       numberToStr += str(s.pop()) 
+
+    return numberToStr
+
+print(devideByTwo(12))
